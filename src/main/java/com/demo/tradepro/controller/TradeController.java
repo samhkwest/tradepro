7d5b47ca66615432352c3e27eb2383e214c0e1a8
samhkwest <samhkwest@gmail.com>
2021-10-09 14:19:23 +0800

    Create TradeController.java

---
+++ src/main/java/com/demo/tradepro/controller/TradeController.java
@@ -0,0 +1,30 @@
+package com.demo.tradepro.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.tradepro.model.Trade;
+import com.demo.tradepro.service.TradeService;
+
+@Component
+@RestController
+public class TradeController {
+	
+  @Autowired
+  private TradeService tradeService;
+      
+  @PostMapping(path="/tradepro/receipt", produces = MediaType.TEXT_PLAIN_VALUE)
+  public ResponseEntity<Object> createReceipt(@RequestBody Trade trade){
+	  //stem.out.println("trade: "+trade.toString());	  
+	  String receipt = tradeService.getReceipt(trade);	  
+	  return new ResponseEntity<Object>(receipt, HttpStatus.OK);
+  }
+  
+ 
+}
