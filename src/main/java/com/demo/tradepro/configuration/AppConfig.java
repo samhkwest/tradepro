8d155fd15a20acfbb238c932e33b73d6b736226a
samhkwest <samhkwest@gmail.com>
2021-10-10 10:07:33 +0800

    Update AppConfig.java

--- src/main/java/com/demo/tradepro/configuration/AppConfig.java
+++ src/main/java/com/demo/tradepro/configuration/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("app")
 public class AppConfig {
 	private List<SalesTax> salestax;
-	private Map<String, String> exemptcat;
+	private Map<String, String> productcat;
 	
 	public void setSalestax(List<SalesTax> salestax) {
 	    this.salestax = salestax;
@@ -20,50 +20,46 @@ public class AppConfig {
         return salestax;
     }
     
-	public void setExemptcat(Map<String, String> exemptcat) {
-	    this.exemptcat = exemptcat;
+	public void setProductcat(Map<String, String> productcat) {
+	    this.productcat = productcat;
     }
     
-    public Map<String, String> getExemptcat() {
-        return exemptcat;
-    }
-	
-    public static class exemptcat {
-    	
+    public Map<String, String> getProductcat() {
+        return productcat;
     }
     
-	public static class SalesTax {
-		private String location;
-		private String exempt;
-		private double rate;
-		
-		public void setLocation(String location) {
-		    this.location = location;
-	    }
-	    
-	    public String getLocation() {
-	        return location;
-	    }
-	    
-		public void setExempt(String exempt) {
-		    this.exempt = exempt;
-	    }
-	    
-	    public String getExempt() {
-	        return exempt;
-	    }
-	    
-		public void setRate(double rate) {
-		    this.rate = rate;
-	    }
-	    
-	    public double getRate() {
-	        return rate;
-	    }
-	    
-	    @Override
-	    public String toString() {
-	    	return String.format("%s, %s, %s", location, exempt, rate);
-	    }
-	}
+    public static class SalesTax {
+	private String location;
+	private String exempt;
+	private double rate;
+
+	public void setLocation(String location) {
+	    this.location = location;
+    }
+
+    public String getLocation() {
+	return location;
+    }
+
+	public void setExempt(String exempt) {
+	    this.exempt = exempt;
+    }
+
+    public String getExempt() {
+	return exempt;
+    }
+
+	public void setRate(double rate) {
+	    this.rate = rate;
+    }
+
+    public double getRate() {
+	return rate;
+    }
+
+    @Override
+    public String toString() {
+	return String.format("%s, %s, %s", location, exempt, rate);
+    }
+}
 }
